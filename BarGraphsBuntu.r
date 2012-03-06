f6619645b2eca3c035894c33c2a312d0575e08f0
Timo Rantalainen <tjrantal@gmail.com>
2012-03-06 13:48:46 +1100

    Added CIs as error whiskers

--- BarGraphsBuntu.r
+++ BarGraphsBuntu.r
@@ -9,6 +9,7 @@ figureTargetPrefix = 'BarGraph_';							#Prefix for figure names.
 source(paste(rFilePath,'getPlotLimits.r',sep=""));
 source(paste(rFilePath,'getTickMarkLabels.r',sep=""));
 source(paste(rFilePath,'errorbar.r',sep=""));
+source(paste(rFilePath,'count.r',sep=""));
 dataIn <- read.table(paste(dataFilePath,dataFileName,sep=""),header=TRUE,sep=',');
 # count.fields('Marrow2011ReAna.csv',sep=',')	#R can't handle ' in header!
 
@@ -43,8 +44,16 @@ for (i in 1:length(variables)){
 
 	averages = aggregate(dataIn[,variables[i]],by=list(dataIn[,"Ryhma"]),FUN=mean, na.rm=TRUE);
 	stdevs = aggregate(dataIn[,variables[i]],by=list(dataIn[,"Ryhma"]),FUN=sd, na.rm=TRUE);
+	Ns = aggregate(dataIn[,variables[i]],by=list(dataIn[,"Ryhma"]),FUN=count);
+	#Calculate CIs based on t-distribution
+	CIs = c();
+	for (g in 1:length(Ns[,2])){
+		tValue = qt(0.975,Ns[g,2]-1);
+		CIs = c(CIs,tValue*stdevs[g,2]/sqrt(Ns[g,2]));
+	}
 	#Get y-axis limits and ticks
-	yLimits <- getPlotLimits(c(c(averages[,2])+c(stdevs[,2]),c(averages[,2])-c(stdevs[,2])),desiredDigits[i],tickDivisions,yXtraSpace[i]);
+	#yLimits <- getPlotLimits(c(c(averages[,2])+c(stdevs[,2]),c(averages[,2])-c(stdevs[,2])),desiredDigits[i],tickDivisions,yXtraSpace[i]);
+	yLimits <- getPlotLimits(c(c(averages[,2])+CIs,c(averages[,2])-CIs),desiredDigits[i],tickDivisions,yXtraSpace[i]);
 	yTick <- getTickMarkLabels(yLimits[1],yLimits[2],desiredDigits[i],tickDivisions);
 	png(paste(figureTargetPath, figureTargetPrefix,100+i,axisTitles[i],'.png', sep = ""),width=1800,height=1200,res=200);	#Create a png to plot to
 	par('mar' = c(3.3,3.8,3.0,1.1),'mgp'=c(2.7, 0.45, 0), 'bg' = pointColor[1],'cex'=2.0);								#Margins bottom, left, top, right
@@ -60,6 +69,6 @@ for (i in 1:length(variables)){
 	);
 	#Plot y-axis
 	axis(2, at=yTick, labels = yTick,las = 2,tck = -0.02);	#Y-axis
-	errorbar(barPlot,c(averages[,2]), c(stdevs[,2]))
+	errorbar(barPlot,c(averages[,2]), CIs)
 	dev.off();
 }
