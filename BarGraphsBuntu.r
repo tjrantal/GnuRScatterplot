c20466800afb9708fee2918d01b8478ab2d47e66
Timo Rantalainen <tjrantal@gmail.com>
2012-03-06 13:18:53 +1100

    adjusting graph looks

--- BarGraphsBuntu.r
+++ BarGraphsBuntu.r
@@ -8,13 +8,14 @@ figureTargetPrefix = 'BarGraph_';							#Prefix for figure names.
 
 source(paste(rFilePath,'getPlotLimits.r',sep=""));
 source(paste(rFilePath,'getTickMarkLabels.r',sep=""));
+source(paste(rFilePath,'errorbar.r',sep=""));
 dataIn <- read.table(paste(dataFilePath,dataFileName,sep=""),header=TRUE,sep=',');
 # count.fields('Marrow2011ReAna.csv',sep=',')	#R can't handle ' in header!
 
 variables = c('StratecMaMassD..g.cm..','Density.weighted.fat.percentage....','MuA..cm..','CoA..mm..',
 			'SSI..mm..','CoD..mg.cm..');
 
-groups = c("High impact","Odd impact","High Magnitude","Repetitive, low impact","Repetitive, non-impact","Referents");
+groups = c("HI","OI","HM","LI","NI","Ref");
 axisTitles = c("MaD","Fat Percentage","Muscle CSA","CoA","SSI","CoD");
 
 
@@ -28,7 +29,7 @@ units = c(
 	);
 
 
-desiredDigits = c(2,2,2,2,2,2,2,2);
+desiredDigits = c(2,2,2,2,2,3,2,2);
 
 
 yXtraSpace = c(0,0,0,0,0,0,0,0);
@@ -43,16 +44,22 @@ for (i in 1:length(variables)){
 	averages = aggregate(dataIn[,variables[i]],by=list(dataIn[,"Ryhma"]),FUN=mean, na.rm=TRUE);
 	stdevs = aggregate(dataIn[,variables[i]],by=list(dataIn[,"Ryhma"]),FUN=sd, na.rm=TRUE);
 	#Get y-axis limits and ticks
-	yLimits <- getPlotLimits(averages,desiredDigits[i],tickDivisions,yXtraSpace[i]);
+	yLimits <- getPlotLimits(c(c(averages[,2])+c(stdevs[,2]),c(averages[,2])-c(stdevs[,2])),desiredDigits[i],tickDivisions,yXtraSpace[i]);
 	yTick <- getTickMarkLabels(yLimits[1],yLimits[2],desiredDigits[i],tickDivisions);
-	png(paste(figureTargetPath, figureTargetPrefix,axisTitles[i],'.png', sep = ""),width=1800,height=1200,res=200);	#Create a png to plot to
-	par('mar' = c(3.3,3.6,3.0,1.1),'mgp'=c(2.2, 0.45, 0), 'bg' = pointColor[1],'cex'=2.0);								#Margins bottom, left, top, right
+	png(paste(figureTargetPath, figureTargetPrefix,100+i,axisTitles[i],'.png', sep = ""),width=1800,height=1200,res=200);	#Create a png to plot to
+	par('mar' = c(3.3,3.8,3.0,1.1),'mgp'=c(2.7, 0.45, 0), 'bg' = pointColor[1],'cex'=2.0);								#Margins bottom, left, top, right
 	#Plot the figure.
 	#barplot(height=averages[,], main=axisTitles[i], horiz=FALSE, names.arg=groups, cex.names=0.8);#,
-	barplot(height=c(averages[,2]), main=axisTitles[i], horiz=FALSE, names.arg=groups, cex.names=0.8);#,
-	#	yaxp = c(yLimits[1],yLimits[2],tickDivisions ), #For determining, xtick length...
-	#	ylab = units[i],
-	#	ylim=c(yLimits[3],yLimits[4])
-	#);
+	barPlot = barplot(height=c(averages[,2]), main=axisTitles[i], horiz=FALSE, names.arg=groups, cex.names=0.8,	
+		yaxp = c(yLimits[1],yLimits[2],tickDivisions ), #For determining, xtick length...
+		ylab = units[i],
+		ylim=c(yLimits[3],yLimits[4]),
+		axes=FALSE,
+		xpd=FALSE,
+		col="white"
+	);
+	#Plot y-axis
+	axis(2, at=yTick, labels = yTick,las = 2,tck = -0.02);	#Y-axis
+	errorbar(barPlot,c(averages[,2]), c(stdevs[,2]))
 	dev.off();
 }
