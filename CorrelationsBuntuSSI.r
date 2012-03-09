397246e9447f36700338bc09723e271070444aa2
Timo Rantalainen <tjrantal@gmail.com>
2012-03-09 16:46:31 +1100

    Modified scatterplotting and added pooled fit to SSI

--- CorrelationsBuntuSSI.r
+++ CorrelationsBuntuSSI.r
@@ -1,4 +1,5 @@
-#Ubuntu execute from R command line source("/media/sf_Deakin/Marrow2011/CorrelationsBuntu.r")
+#Ubuntu execute from R command line 
+#source("/media/sf_Deakin/Marrow2011/CorrelationsBuntuSSI.r")
 
 dataFilePath = 'H:/UserData/winMigrationBU/Deakin/Marrow2011/';	#Path to the data file.
 dataFilePath = '/home/timo/Desktop/Media/sf_Deakin/Marrow2011/';	#Path to the data file.
@@ -50,7 +51,7 @@ yunits = c(
 yDesiredDigits = c(2,2,2,2,2,2,2,2);
 xDesiredDigits = c(3,3,3,3,3,3,3,3);
 
-yXtraSpace = c(0,0,0,0,7,0,0,0);
+yXtraSpace = c(0,0,0,0,1,0,5,0);
 xXtraSpace = c(1,1,1,1,1,1,1,1);
 
 
@@ -58,7 +59,7 @@ tickDivisions = 2;
 pointColor  = c("#ffffff","#000000","#000000");
 
 for (j in 1:length(xVariables)){
-	for(i in 1:length(yVariables)){			#Loop to going through the yVariables
+	for(i in 7){#:length(yVariables)){			#Loop to going through the yVariables
 		#Create figure to plot to
 		#Get x-axis limits and ticks
 		xLimits <- getXPlotLimits(dataIn[xVariables[j]],xDesiredDigits[i],tickDivisions,xXtraSpace[i]);
@@ -71,8 +72,8 @@ for (j in 1:length(xVariables)){
 		par('mar' = c(3.3,3.6,3.0,1.1),'mgp'=c(2.2, 0.45, 0), 'bg' = pointColor[1],'cex'=2.0);								#Margins bottom, left, top, right
 		for (g in 1:length(groupedData)){
 			groupData = groupedData[[g]];
-			y = groupData[yVariables[i]];			#Get y-axis data
-			x = groupData[xVariables[j]];				#Get x-axis data
+			x = dataIn[xVariables[j]];
+			y = dataIn[yVariables[i]];			
 			pearsonR = cor(x,y);
 			myline.fit <- lm(y[,1] ~ x[,1]);	#Get linear fit, i.e. correlation
 			summaryData <- summary(myline.fit); # get information about the fit
@@ -81,6 +82,8 @@ for (j in 1:length(xVariables)){
 			int =  summaryData$coefficients[1,1];
 			Rsq =  summaryData$r.squared[1];
 			
+			y = groupData[yVariables[i]];			#Get y-axis data
+			x = groupData[xVariables[j]];				#Get x-axis data
 
 			#Plot the figure.
 			if (g == 1){
@@ -105,27 +108,29 @@ for (j in 1:length(xVariables)){
 				points(x[,1],y[,1],type='p', pch=21,col = pointColor[g+1],bg ="white");
 			}
 			#Add P-values, R2 and if significant, linear fit line.
-			if (pValue <= 0.05){
-				abline(lwd = 5.0,myline.fit,col = pointColor[g+1],lty =g+1) # draw the fit line on the plot
+			if (g ==1){
+				if (pValue <= 0.05){
+					abline(lwd = 5.0,myline.fit,col = pointColor[g+1],lty =g) # draw the fit line on the plot
+				}
+				textXpos = c((xLimits[4]-xLimits[3])*0.9+xLimits[3],(xLimits[4]-xLimits[3])*0.8+xLimits[3]);
+				PText = "";
+				if (pValue <= 0.05){
+					if (pValue < 0.001){
+						PText = "P < 0.001";
+					}
+					if (pValue < 0.01 && pValue >= 0.001){
+						PText = "P < 0.01";
+					}
+					if (pValue <= 0.05 && pValue >= 0.01){
+						PText = "P < 0.05";
+					}
+				}else{
+					PText = paste("P = ",format(round(pValue,digits=2),nsmall =2,digits =2),sep="");
+				}
+				RText = paste("=",format(round(Rsq,digits=2),nsmall=2,digits=2));
+				#mtext(substitute(paste(R^2,ya,", ",yb,sep=""),list(ya=RText,yb=PText)), side = 3, line = g-1,col = pointColor[g+1],adj = 1,cex = 1.8);
+				text(textXpos[2] ,yLimits[3]+(yLimits[4]-yLimits[3])*0.1*(g-1),substitute(paste(R^2,ya,", ",yb,sep=""),list(ya=RText,yb=PText)),pos=3,offset=0,col = pointColor[g+1]);
 			}
-			textXpos = c((xLimits[4]-xLimits[3])*0.9+xLimits[3],(xLimits[4]-xLimits[3])*0.8+xLimits[3]);
-			PText = "";
-			if (pValue <= 0.05){
-				if (pValue < 0.001){
-					PText = "P < 0.001";
-				}
-				if (pValue < 0.01 && pValue >= 0.001){
-					PText = "P < 0.01";
-				}
-				if (pValue <= 0.05 && pValue >= 0.01){
-					PText = "P < 0.05";
-				}
-			}else{
-				PText = paste("P = ",format(round(pValue,digits=2),nsmall =2,digits =2),sep="");
-			}
-			RText = paste("=",format(round(Rsq,digits=2),nsmall=2,digits=2));
-			#mtext(substitute(paste(yc,R^2,ya,", ",yb,sep=""),list(ya=RText,yb=PText,yc=groupSymbols[g])), side = 3, line = g-1,col = pointColor[g+1],adj = 1,cex = 1.8);
-			text(textXpos[2] ,yLimits[3]+(yLimits[4]-yLimits[3])*0.1*(g-1),substitute(paste(yc,R^2,ya,", ",yb,sep=""),list(ya=RText,yb=PText,yc=groupSymbols[g])),pos=3,offset=0,col = pointColor[g+1]);
 		}
 	dev.off();
 	}
--- getXPlotLimits.r
+++ getXPlotLimits.r
@@ -4,13 +4,13 @@
 #digits:	How many significant figures are required
 #tickMarks:	How many tickmarks are required
 #returns:	An array [lowerLimit,upperLimit,lowerTickLimit,upperTickLimit]
-getPlotLimits <- function(dataIn,digitsIn,tickMarks,xtraSpace) { #DEBUG dataIn =data[xVariable] digitsIn=xDesiredDigits[i] tickMarks=tickDivisions xtraSpace=xXtraSpace[i]
+getXPlotLimits <- function(dataIn,digitsIn,tickMarks,xtraSpace) { #DEBUG dataIn =data[xVariable] digitsIn=xDesiredDigits[i] tickMarks=tickDivisions xtraSpace=xXtraSpace[i]
 	exponent= floor(log10(max(dataIn,na.rm=TRUE)));
 	multiplier = 10^((digitsIn-1)-exponent);
 	lowerLimit = floor(min(dataIn,na.rm=TRUE)*multiplier )/multiplier;
 	lowerTickLimit = lowerLimit-xtraSpace/multiplier;#;# 
 	upperLimit = ceiling(max(dataIn,na.rm=TRUE)*multiplier )/multiplier;
-	upperTickLimit = upperLimit#+xtraSpace/multiplier;#;# 
+	upperTickLimit = upperLimit+xtraSpace/multiplier;#;# 
 	if ((upperTickLimit*multiplier -lowerTickLimit*multiplier )%%tickMarks!= 0){
 		upperTickLimit = (upperTickLimit*multiplier +tickMarks-((upperTickLimit*multiplier -lowerTickLimit*multiplier )%%tickMarks))/multiplier; 
 	}
